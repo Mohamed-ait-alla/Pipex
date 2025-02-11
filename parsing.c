8b969625b8dbcbd31a45c96b1781244a5cb46ada
Mohamed Ait Alla <mait-all@c3r1p1.1337.ma>
2025-02-11 09:37:58 +0100

    Update code: add a check when a cmd is in the working directory

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:30:40 by mait-all          #+#    #+#             */
-/*   Updated: 2025/02/10 17:46:46 by mait-all         ###   ########.fr       */
+/*   Updated: 2025/02/10 21:34:17 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ char	*get_exec_path(char **env, char *cmd)
 	char	**env_paths;
 	int		i;
 
+	if (cmd && (cmd[0] == '.' || cmd[0] == '/'))
+	{
+		exec_path = ft_memchr(cmd, '/', ft_strlen(cmd));
+		if (access(exec_path, F_OK) == 0 && access(exec_path, X_OK) == 0)
+			return (exec_path);
+	}
 	path = ft_memchr(env[19], '/', ft_strlen(env[19]));
 	env_paths = ft_split(path, ':');
 	i = 0;
