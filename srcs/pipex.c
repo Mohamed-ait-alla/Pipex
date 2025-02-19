6a5690118c630440e42ebfce96d8e43231697403
Mohamed Ait Alla <mait-all@c3r6p1.1337.ma>
2025-02-19 17:48:53 +0100

    Update mandatory part: fix a bug with exit status codes so the pipex program will exit with the last child's exit status code to match shell behavior

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:28:04 by mait-all          #+#    #+#             */
-/*   Updated: 2025/02/19 15:30:50 by mait-all         ###   ########.fr       */
+/*   Updated: 2025/02/19 17:46:15 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv, char **env)
 	int	i;
 	char **args;
 	char *path;
+	int status;
 
 	// check arguments
 	if (argc != 5)
@@ -85,29 +86,21 @@ int	main(int argc, char **argv, char **env)
 					ft_printf("./pipex: no such file or directory: %s\n", argv[2]);
 					exit(127);
 				}
-    			redirect_input_from_pipe(fd[0]);
-    			redirect_output_to_file(argv[4]);
-    			close(fd[0]);
-    			close(fd[1]);
-    			execute_command(argv[3], env);
+				redirect_input_from_pipe(fd[0]);
+				redirect_output_to_file(argv[4]);
+				close(fd[0]);
+				close(fd[1]);
+				execute_command(argv[3], env);
 			}
 		}
 		i++;
 	}
 	close (fd[0]);
 	close (fd[1]);
-	// waitpid (pids[0], NULL, 0);
-	int status;
-	waitpid(pids[0], &status, 0);  // Wait for the first child
-	if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-    // First child failed, stop further execution
-    	exit(WEXITSTATUS(status));
-	}
-// waitpid(pids[1], NULL, 0);  // Wait for second child
-
+	waitpid(pids[0],NULL, 0);  
 	waitpid (pids[1], &status, 0);
 	if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-    // First child failed, stop further execution
-    	exit(WEXITSTATUS(status));
+		// exit with last child's exit code
+		exit(WEXITSTATUS(status));
 	}
 }
